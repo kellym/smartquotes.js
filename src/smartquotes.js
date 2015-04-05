9742ca5200dc5ed1da7e1c107e46486c2a59f5bd
Callum Macrae <callum@macr.ae>
2015-04-05 14:17:14 +0100

    Refactored library for more use cases

--- src/smartquotes.js
+++ src/smartquotes.js
@@ -6,42 +6,69 @@
  * Copyright (C) 2013 Kelly Martin, http://kelly-martin.com
  */
 
-(typeof('jQuery') == 'function' ? jQuery : function ( callback ) {
-  var addListener = document.addEventListener || document.attachEvent,
-      removeListener = document.removeEventListener ? "removeEventListener" : "detachEvent",
-      eventName = document.addEventListener ? "DOMContentLoaded" : "onreadystatechange";
-
-  addListener.call(document, eventName, function() {
-    document[removeListener](eventName, arguments.callee, false);
-    callback();
-  }, false);
-})(function() {
-  var root = document.body;
-  var node = root.childNodes[0];
-  while(node != null) {
-    if(node.nodeType == 3) {
-      node.nodeValue = node.nodeValue
-       .replace(/(\W|^)"(\S)/g, '$1\u201c$2')                                       // beginning "
-       .replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2')          // ending "
-       .replace(/([^0-9])"/g,'$1\u201d')                                            // remaining " at end of word
-       .replace(/(\W|^)'(\S)/g, '$1\u2018$2')                                       // beginning '
-       .replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                  // conjunction's possession
-       .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3')                 // ending '
-       .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3')     // abbrev. years like '93
-       .replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
-       .replace(/'''/g, '\u2034')                                                   // triple prime
-       .replace(/("|'')/g, '\u2033')                                                // double prime
-       .replace(/'/g, '\u2032');                                                    // prime
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	} else {
+		root.smartquotes = factory();
+	}
+}(this, function () {
+  // The smartquotes should just delegate to the other functions
+  function smartquotes(context) {
+    if (typeof context === 'undefined') {
+      return smartquotes.page();
     }
-    if(node.hasChildNodes() && node.firstChild.nodeName != "CODE") {
-      node = node.firstChild;
-    } else {
-      do {
-        while(node.nextSibling == null && node != root) {
-          node = node.parentNode;
-        }
-        node = node.nextSibling;
-      } while (node && (node.nodeName == "CODE" || node.nodeName == "SCRIPT" || node.nodeName == "STYLE"));
+
+    if (typeof context === 'string') {
+      return smartquotes.string(context);
+    }
+    
+    if (context instanceof HTMLElement) {
+      return smartquotes.html(context);
     }
   }
-});
+
+  smartquotes.string = function smartquotesString(str) {
+    return str
+      .replace(/(\W|^)"(\S)/g, '$1\u201c$2')                                       // beginning "
+      .replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2')          // ending "
+      .replace(/([^0-9])"/g,'$1\u201d')                                            // remaining " at end of word
+      .replace(/(\W|^)'(\S)/g, '$1\u2018$2')                                       // beginning '
+      .replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                  // conjunction's possession
+      .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3')                 // ending '
+      .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3')     // abbrev. years like '93
+      .replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
+      .replace(/'''/g, '\u2034')                                                   // triple prime
+      .replace(/("|'')/g, '\u2033')                                                // double prime
+      .replace(/'/g, '\u2032');
+  };
+
+  smartquotes.html = function smartquotesHtml(root) {
+    var node = root.childNodes[0];
+
+    while (node !== null) {
+      if (node.nodeType === 3) {
+        node.nodeValue = smartquotes.string(node.nodeValue);
+      }
+
+      if (node.hasChildNodes() && node.firstChild.nodeName !== 'CODE') {
+        node = node.firstChild;
+      } else {
+        do {
+          while (node.nextSibling === null && node !== root) {
+            node = node.parentNode;
+          }
+          node = node.nextSibling;
+        } while (node && ['CODE', 'SCRIPT', 'STYLE'].indexOf(node.nodeName) !== -1);
+      }
+    }
+  };
+
+  smartquotes.page = function smartquotesPage() {
+    smartquotes.html(document.body);
+  };
+
+  return smartquotes;
+}));
