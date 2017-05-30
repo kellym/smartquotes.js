365751359ea7d7b6fc18d3e6116a71374d39f5fd
Kelly <kellym@users.noreply.github.com>
2017-05-29 22:14:02 -0500

    Fix single quotes when abbreviation exists and fix quote surrounded by non-word (#22)

--- test/smartquotes.js
+++ test/smartquotes.js
@@ -2,28 +2,38 @@ var jsdom = require('jsdom');
 var test = require('tap').test;
 var smartquotes = require('../');
 
-test('smartquotes.string()', function (t) {
+// a list of test strings and expected converted values
+var expectations = {
+  '"test"': '\u201ctest\u201d',
+  'the\u2014 "test"': 'the\u2014 \u201ctest\u201d',
+  '\'test\'': '\u2018test\u2019',
+  'ma\'am': 'ma\u2019am',
+  '\'em': '\u2019em',
+  'Marshiness of \'Ammercloth\'s': 'Marshiness of \u2019Ammercloth\u2019s',
+  '\'95': '\u201995',
+  '\'\'\'': '\u2034',
+  '\'\'': '\u2033',
+  '"Better than a 6\'5" whale."': '\u201cBetter than a 6\u20325\u2033 whale.\u201d',
+  '"It\'s my \'#1\' choice!" - 12" Foam Finger from \'93': '\u201cIt\u2019s my \u2018#1\u2019 choice!\u201d - 12\u2033 Foam Finger from \u201993',
+  '"Say \'what?\'" says a Mill\'s Pet Barn employee.': '\u201cSay \u2018what?\u2019\u201d says a Mill\u2019s Pet Barn employee.',
+  '"Quote?": Description': '\u201cQuote?\u201d: Description',
+  '\'Quo Te?\': Description': '\u2018Quo Te?\u2019: Description',
+  '"De Poesjes van Kevin?": Something, something': '\u201cDe Poesjes van Kevin?\u201d: Something, something',
+  'And then she blurted, "I thought you said, \'I don\'t like \'80s music\'?"': "And then she blurted, \u201cI thought you said, \u2018I don\u2019t like \u201980s music\u2019?\u201d"
+};
+
+test('smartquotes.string()', (t) => {
+
   var s = smartquotes.string;
-  t.equal(s('"test"'), '\u201ctest\u201d');
-  t.equal(s('the\u2014 "test"'), 'the\u2014 \u201ctest\u201d');
-  t.equal(s('\'test\''), '\u2018test\u2019');
-  t.equal(s('ma\'am'), 'ma\u2019am');
-  t.equal(s('\'em'), '\u2019em');
-  t.equal(s('Marshiness of \'Ammercloth\'s'), 'Marshiness of \u2019Ammercloth\u2019s');
-  t.equal(s('\'95'), '\u201995');
-  t.equal(s('\'\'\''), '\u2034');
-  t.equal(s('\'\''), '\u2033');
-  t.equal(s('"Better than a 6\'5" whale."'), '\u201cBetter than a 6\u20325\u2033 whale.\u201d');
-  t.equal(s('"It\'s my \'#1\' choice!" - 12" Foam Finger from \'93'), '\u201cIt\u2019s my \u2018#1\u2019 choice!\u201d - 12\u2033 Foam Finger from \u201993');
-  t.equal(s('"Say \'what?\'" says a Mill\'s Pet Barn employee.'), '\u201cSay \u2018what?\u2019\u201d says a Mill\u2019s Pet Barn employee.');
-  t.equal(s('"Quote?": Description'), '\u201cQuote?\u201d: Description');
-  t.equal(s('\'Quote?\': Description'), '\u2018Quote?\u2019: Description');
+  Object.keys(expectations).forEach((string) => {
+    t.equal(s(string), expectations[string]);
+  });
 
   // All synchronous, no need for t.plan()
   t.end();
 });
 
-test('smartquotes.element()', function (t) {
+test('smartquotes.element()', (t) => {
   t.plan(3);
 
   jsdom.env({
